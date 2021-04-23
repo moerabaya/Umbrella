3944b1be88181d6dfc2c5389df8e0730f2982c45
Mohammed Rabaya <mrabaya@cura.healthcare>
2021-04-23 11:49:09 +0300

    Initial Commit

---
+++ Umbrella/UmbrellaApp.swift
@@ -0,0 +1,17 @@
+//
+//  UmbrellaApp.swift
+//  Umbrella
+//
+//  Created by Mohammed Rabaya on 23/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct UmbrellaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
